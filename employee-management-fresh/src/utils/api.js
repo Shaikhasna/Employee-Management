54438fb107bbf20352fa5778ecea18933e2bcd86
shaikhasna <hasnashaik25@gmail.com>
2024-12-20 22:20:31 +0530

    Fix build issues and update dependencies

--- employee-management-fresh/src/utils/api.js
+++
@@ -1,6 +0,0 @@
-fetch('http://localhost:5004/api/employees')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => console.error('Error:', error));
